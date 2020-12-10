901929fa0057ec2b2392ed5ae0084829a82f0e60
Candice Zhao <71113347+candiiice5@users.noreply.github.com>
2020-12-09 23:19:45 -0500

    Update GameStart.java

--- src/main/Poker/GameStart.java
+++ src/main/Poker/GameStart.java
@@ -3,9 +3,25 @@ package Poker;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Scanner;
+
 public class GameStart {
+	
+	public int getBiggestHand(List<Player> players) {
+		int biggestHand = players.get(0).getIndDeck().size();
+		int pBigHand = 0;
+		for(int i = 0; i <= 3; i++) {
+			if(players.get(i).getIndDeck().size() > biggestHand) {
+				pBigHand = i;
+			}
+		}
+		return pBigHand;
+	}
+	
 	public static void main(String[] args) {
 		GameStart round = new GameStart();
+		
+		Scanner input = new Scanner(System.in);
 		
 		Deck deck = new Deck();
 		
@@ -20,8 +36,52 @@ public class GameStart {
 			for(int j = 0; j <= 12; j++) {
 				players.get(i).setIndDeck(deck.getDeck().get((i+ 1) * (j + 1) - 1));
 			}
-			players.get(i).showIndDeck();
+			//players.get(i).showIndDeck();
 			players.get(i).removeDuplicates();
 		}
+		
+		int pBigHand = round.getBiggestHand(players);
+		
+		//System.out.println(round.getBiggestHand(players));
+		
+		for(int i = pBigHand; i <= pBigHand + 3; i++) {
+			if(i > 3) {
+				players.get(i - 4).showIndDeck();
+				
+				System.out.println("Choose from 1 to " + players.get(i - 4).getIndDeck().size());
+				
+				int number = input.nextInt();
+				
+				Card cardPicked = players.get(i - 4).getIndDeck().get(number - 1);
+				
+				players.get(i - 3).setIndDeck(cardPicked);
+				
+				System.out.println(cardPicked.getSuit() + cardPicked.getCardNum());
+			}
+			else {
+				players.get(i).showIndDeck();
+				
+				System.out.println("Choose from 1 to " + players.get(i).getIndDeck().size());
+				
+				int number = input.nextInt();
+				
+				Card cardPicked = players.get(i).getIndDeck().get(number - 1);
+				
+				if(i == 3) {
+					players.get(0).setIndDeck(cardPicked);
+				}
+				else {
+					players.get(i + 1).setIndDeck(cardPicked);
+				}
+				
+				System.out.println(cardPicked.getSuit() + cardPicked.getCardNum());
+			}
+		}
+		
+		for(int i = 0; i <= 3; i++) {
+			players.get(i).showIndDeck();
+		}
+		
+		input.close();
 	}
 }
