8f320dac333f0d9504596aa3cb8959a66fc4d488
Candice Zhao <71113347+candiiice5@users.noreply.github.com>
2020-12-10 23:11:04 -0500

    Update GameStart.java

--- src/main/Poker/GameStart.java
+++ src/main/Poker/GameStart.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 public class GameStart {
 	
+	private boolean IsGameEnd;
+	
 	public int getBiggestHand(List<Player> players) {
 		int biggestHand = players.get(0).getIndDeck().size();
 		int pBigHand = 0;
@@ -16,6 +18,10 @@ public class GameStart {
 			}
 		}
 		return pBigHand;
+	}
+	
+	public boolean IsGameEnd(Player playerUse) {
+		return playerUse.getIndDeck().isEmpty();
 	}
 	
 	public static void main(String[] args) {
@@ -44,44 +50,101 @@ public class GameStart {
 		
 		//System.out.println(round.getBiggestHand(players));
 		
-		for(int i = pBigHand; i <= pBigHand + 3; i++) {
-			if(i > 3) {
-				players.get(i - 4).showIndDeck();
-				
-				System.out.println("Choose from 1 to " + players.get(i - 4).getIndDeck().size());
-				
-				int number = input.nextInt();
-				
-				Card cardPicked = players.get(i - 4).getIndDeck().get(number - 1);
-				
-				players.get(i - 3).setIndDeck(cardPicked);
-				
-				System.out.println(cardPicked.getSuit() + cardPicked.getCardNum());
-			}
-			else {
-				players.get(i).showIndDeck();
-				
-				System.out.println("Choose from 1 to " + players.get(i).getIndDeck().size());
-				
-				int number = input.nextInt();
-				
-				Card cardPicked = players.get(i).getIndDeck().get(number - 1);
-				
-				if(i == 3) {
-					players.get(0).setIndDeck(cardPicked);
+		while(!round.IsGameEnd) {
+			for(int i = pBigHand; i <= pBigHand + 3; i++) {
+				if(i > 3) {
+					
+					players.get(i - 4).showIndDeck();
+					
+					System.out.println("Choose from 1 to " + players.get(i - 4).getIndDeck().size());
+					
+					int number = input.nextInt();
+					
+					Card cardPicked = players.get(i - 4).getIndDeck().get(number - 1);
+					
+					players.get(i - 4).removeCard(number);
+					
+					if(round.IsGameEnd(players.get(i - 4))) {
+						System.out.println("Winner is Player # " + (i - 4));
+						
+						round.IsGameEnd = true;
+						
+						break;
+					}
+					
+					players.get(i - 3).setIndDeck(cardPicked);
+					
+					System.out.println(cardPicked.getSuit() + cardPicked.getCardNum());
+					
+					players.get(i - 3).removeDuplicates();
+					
+					if(round.IsGameEnd(players.get(i - 3))) {
+						System.out.println("Winner is Player # " + (i - 3));
+						
+						round.IsGameEnd = true;
+						
+						break;
+					}
 				}
 				else {
-					players.get(i + 1).setIndDeck(cardPicked);
+					players.get(i).showIndDeck();
+					
+					System.out.println("Choose from 1 to " + players.get(i).getIndDeck().size());
+					
+					int number = input.nextInt();
+					
+					Card cardPicked = players.get(i).getIndDeck().get(number - 1);
+					
+					System.out.println(cardPicked.getSuit() + cardPicked.getCardNum());
+					
+					players.get(i).removeCard(number);
+					
+					if(round.IsGameEnd(players.get(i))) {
+						System.out.println("Winner is Player # " + (i));
+						
+						round.IsGameEnd = true;
+						
+						break;
+					}
+					
+					if(i == 3) {
+						players.get(0).setIndDeck(cardPicked);
+						
+						players.get(0).removeDuplicates();
+						
+						if(round.IsGameEnd(players.get(0))) {
+							System.out.println("Winner is Player # " + (0));
+							
+							round.IsGameEnd = true;
+							
+							break;
+						}
+					}
+					else {
+						players.get(i + 1).setIndDeck(cardPicked);
+						
+						players.get(i + 1).removeDuplicates();
+						
+						if(round.IsGameEnd(players.get(i + 1))) {
+							System.out.println("Winner is Player # " + (i + 1));
+							
+							round.IsGameEnd = true;
+							
+							break;
+						}
+					}
+					
+					
 				}
-				
-				System.out.println(cardPicked.getSuit() + cardPicked.getCardNum());
 			}
+			
+			for(int i = 0; i <= 3; i++) {
+				players.get(i).showIndDeck();
+			}
+			
+			
 		}
-		
-		for(int i = 0; i <= 3; i++) {
-			players.get(i).showIndDeck();
-		}
-		
 		input.close();
 	}
+	
 }
